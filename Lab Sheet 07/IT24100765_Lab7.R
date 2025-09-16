14266d8118df1cf837b4003bada209fde4ec2c50
IT24100765 <it24100765@my.sliit.lk>
2025-09-16 21:32:30 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24100765_Lab7.R
@@ -0,0 +1,22 @@
+setwd("C://Users//MSI//Desktop//IT24100765")
+
+# Uniform(0,40)
+(25-10)/40
+
+#2
+
+lambda <- 1/3
+pexp(2, rate= lambda)
+
+
+#3(i)
+
+1- pnorm(130,mean=100, sd=15)
+
+#ii
+
+qnorm(0.95, mean=100, sd=15)
+
+
+
+
